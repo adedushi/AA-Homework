fe390ee8f074afbd69f0d303cf6f68896819af36
Albert Dedushi <adedushi@hotmail.com>
2023-11-04 18:58:57 -0400

    Create #initialize for GraphNode

---
+++ W4D1/graph_nodes.rb
@@ -0,0 +1,30 @@
+class GraphNode
+    attr_reader :neighbors, :value
+    attr_writer :neighbors
+
+    def initialize(value)
+        @value = value
+        @neighbors = Array.new
+    end
+
+end
+
+a = GraphNode.new('a')
+b = GraphNode.new('b')
+c = GraphNode.new('c')
+d = GraphNode.new('d')
+e = GraphNode.new('e')
+f = GraphNode.new('f')
+a.neighbors = [b, c, e]
+c.neighbors = [b, d]
+e.neighbors = [a]
+f.neighbors = [e]
+
+
+print a.neighbors
+print c.neighbors
+print e.neighbors
+print f.neighbors
+
+def bfs(starting_node, target_value)
+end
