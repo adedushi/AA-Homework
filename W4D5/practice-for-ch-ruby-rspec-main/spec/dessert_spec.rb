e1e99f0e31d580d2c996e21c0252eb16f3be2c82
Albert Dedushi <adedushi@hotmail.com>
2023-11-09 21:24:38 -0500

    Added tests to dessert_spec

--- W4D5/practice-for-ch-ruby-rspec-main/spec/dessert_spec.rb
+++ W4D5/practice-for-ch-ruby-rspec-main/spec/dessert_spec.rb
@@ -6,24 +6,33 @@ Instructions: implement all of the pending specs (the `it` statements without bl
 =end
 
 describe Dessert do
-  # subject(:dessert) { Desert.new('cookie', 2, 'john')}
   let(:chef) { double('chef') }
+  subject(:cookie) { Dessert.new('cookie', 2, chef)}
 
   describe "#initialize" do
-    it "sets a type"
-    expect(dessert.type).to eq("cookie")
+    it "sets a type" do
+      expect(cookie.type).to eq("cookie")
+    end
 
-    it "sets a quantity"
-    expect(dessert.quantity).to eq(2)
+    it "sets a quantity" do
+      expect(cookie.quantity).to eq(2)
+    end
 
-    it "starts ingredients as an empty array"
-    expect(dessert.ingredients).to eq([])
+    it "starts ingredients as an empty array" do
+      expect(cookie.ingredients).to eq([])
+    end
 
-    it "raises an argument error when given a non-integer quantity"
-  end
+    it "raises an argument error when given a non-integer quantity" do
+      expect { Dessert.new('cookie', 'apple', chef)}.to raise_error(ArgumentError)
+    end
+end
 
   describe "#add_ingredient" do
-    it "adds an ingredient to the ingredients array"
+    it "adds an ingredient to the ingredients array" do
+      expect(cookie.ingredients).to eq([])
+      cookie.add_ingredient('chocolate')
+      expect(cookie.ingredients).to include('chocolate')
+    end
   end
 
   describe "#mix!" do
@@ -31,9 +40,15 @@ describe Dessert do
   end
 
   describe "#eat" do
-    it "subtracts an amount from the quantity"
+    it "subtracts an amount from the quantity" do
+      expect(cookie.quantity).to eq(2)
+      cookie.eat(1)
+      expect(cookie.quantity).to eq(1)
+    end
 
-    it "raises an error if the amount is greater than the quantity"
+    it "raises an error if the amount is greater than the quantity" do
+      expect{cookie.eat(3)}.to raise_error("not enough left!")
+    end
   end
 
   describe "#serve" do
@@ -41,6 +56,9 @@ describe Dessert do
   end
 
   describe "#make_more" do
-    it "calls bake on the dessert's chef with the dessert passed in"
+    it "calls bake on the dessert's chef with the dessert passed in" do
+      
+    end
+
   end
 end
