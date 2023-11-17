c5631e03c115f7c3d6b175e991ad40a821f9440f
Albert Dedushi <adedushi@hotmail.com>
2023-11-16 21:43:32 -0500

    Complete movie queries

--- W5D5/practice-for-ch-sql-movie-buff-in-training-main/movie_novice/queries.rb
+++ W5D5/practice-for-ch-sql-movie-buff-in-training-main/movie_novice/queries.rb
@@ -29,20 +29,19 @@
 
 
 def find_angelina
-  # find Angelina Jolie by name in the actors table
-
+  Actor.find_by(name: 'Angelina Jolie')
 end
 
 def top_titles
   # get movie titles from movies with scores greater than or equal to 9
   # hint: use 'select' and 'where'
-  
+  Movie.select(:title).where('score >= 9')
 end
 
 def star_wars
   # display the id, title, and year of each Star Wars movie in movies
   # hint: use 'select' and 'where'
-  
+  Movie.select(:id, :title, :yr).where("title LIKE 'Star Wars%\'")
 end
 
 
@@ -50,21 +49,21 @@ def below_average_years
   # display each year with movies scoring under 5, with the count of movies
   # scoring under 5 aliased as bad_movies, in descending order
   # hint: use 'select', 'where', 'group', 'order'
-  
+  Movie.select('Count(yr) AS bad_movies', :yr).where('score < 5').group('yr').order('bad_movies DESC')
 end
 
 def alphabetized_actors
   # display the id and name of the second 10 actor names (i.e., #s 11-20)
   # when ordered from A-Z
   # hint: use 'order' and 'limit'
-  
+  Actor.select("actors.id, actors.name").order('name ASC').offset(10).limit(10)
 end
 
 def pulp_fiction_actors
   # practice using joins
   # display the id and name of all actors in the movie Pulp Fiction
   # hint: use 'select', 'joins', 'where'
-  
+  Actor.select(:id, :name).joins(:movies).where("movies.title LIKE 'Pulp Fiction'")
 end
 
 def uma_movies
@@ -72,5 +71,5 @@ def uma_movies
   # display the id, title, and year of movies Uma Thurman has acted in
   # order them by ascending year
   # hint: use 'select', 'joins', 'where', and 'order'
-  
+  Movie.select('movies.id, movies.title, movies.yr').joins(:actors).where("actors.name LIKE 'Uma Thurman'").order('yr ASC')
 end
