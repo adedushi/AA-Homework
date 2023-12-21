12c3a795292e837ef92c89be334b8b9f64f218e8
Albert Dedushi <adedushi@hotmail.com>
2023-12-21 10:29:51 -0500

    Updates to template

--- W10D4/my-react-vite-template/src/App.jsx
+++ W10D4/my-react-vite-template/src/App.jsx
@@ -1,5 +1,10 @@
-function App() {
-  return <h1> Hello from App </h1>;
+function App(props) {
+
+  return (
+    <div className="app">
+    <h1>Hello from App</h1>
+    </div>
+  )
 }
 
 export default App;
